ec4d35466540d5d40d28407c124ecc04445ee5ba
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2024-02-06 09:41:02 +0300

    Добавлены файлы

---
+++ quest18.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace quest18._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ quest18.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace quest18._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
